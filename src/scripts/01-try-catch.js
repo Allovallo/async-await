5dc4ccac5976995b82c655c14b990e358785e109
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-01-22 21:07:25 +0200

    .js files updated

---
+++ src/scripts/01-try-catch.js
@@ -0,0 +1,9 @@
+try {
+  console.log('inside try{} till myVar');
+  myVar;
+  console.log('inside try{} after myVar');
+} catch {
+  console.log('ERROR!!!');
+}
+
+console.log('After try...catch!');
---
+++ src/scripts/02-json-parse.js
@@ -0,0 +1,6 @@
+const validJSON = '{"name": "Mango", "age": 3}';
+const invalidJSON = '{бекенд повернув таке чудо}';
+
+console.log(JSON.parse(1));
+console.log(JSON.parse(invalidJSON));
+console.log(JSON.parse(2));
